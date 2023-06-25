78565d8bedd175c9d49d4139fa45a832dc3ff5c4
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-25 21:02:34 +0530

    index signature

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,7 @@
 "use strict";
-class Account {
-    constructor(id, owner, _balance) {
-        this.id = id;
-        this.owner = owner;
-        this._balance = _balance;
-    }
-    deposit(amount) {
-        if (amount <= 0) {
-            throw new Error("invalid amount");
-        }
-        this._balance += amount;
-    }
-    get balance() {
-        return this._balance;
-    }
-    set balance(value) {
-        if (value < 0) {
-            throw new Error('invalid value');
-        }
-        this._balance = value;
-    }
+class SearAssigment {
 }
-let account = new Account(1, "Hanish", 10);
-account.balance = 1;
-console.log(account.balance);
+let seats = new SearAssigment();
+seats.A1 = 'muhd';
+seats.A2 = 'hanish';
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,7 @@
-class Account {
-  nickName?: string;
-  constructor(
-    public readonly id: number,
-    public owner: string,
-    private _balance: number
-  ) {}
-  deposit(amount: number): void {
-    if (amount <= 0) {
-      throw new Error("invalid amount");
-    }
-    // Record a transaction
-    this._balance += amount;
-  }
-  get balance(): number {
-    return this._balance;
-  }
-  set balance(value: number) {
-    if (value < 0) {
-      throw new Error('invalid value');
-    }
-    this._balance = value;
-  }
+class SearAssigment {
+  [seatNumber: string]: string;
 }
 
-let account = new Account(1, "Hanish", 10);
-account.balance = 1;
-console.log(account.balance);
+let seats = new SearAssigment();
+seats.A1 = 'muhd';
+seats.A2 = 'hanish';
