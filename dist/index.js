c823e4aa6ed2fdf612290d469418bae55746ca94
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-25 21:15:00 +0530

    static members

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,15 @@
 "use strict";
-class SearAssigment {
+class Ride {
+    start() { Ride._activeRides++; }
+    stop() { Ride._activeRides--; }
+    static get activeRides() {
+        return Ride._activeRides;
+    }
 }
-let seats = new SearAssigment();
-seats.A1 = 'muhd';
-seats.A2 = 'hanish';
+Ride._activeRides = 0;
+let ride1 = new Ride();
+ride1.start();
+let ride2 = new Ride();
+ride2.start();
+console.log(Ride.activeRides);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-class SearAssigment {
-  [seatNumber: string]: string;
+class Ride {
+  private static _activeRides: number = 0;
+  start() { Ride._activeRides++ }
+  stop() { Ride._activeRides--; }
+  static get activeRides(): number {
+    return Ride._activeRides;
+  }
 }
 
-let seats = new SearAssigment();
-seats.A1 = 'muhd';
-seats.A2 = 'hanish';
+let ride1 = new Ride();
+ride1.start()
+
+let ride2 = new Ride();
+ride2.start();
+
+console.log(Ride.activeRides);
