1b054ead563487e335273842424633d855054aa4
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-25 12:57:31 +0530

    optional chaining

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,7 @@
 "use strict";
-function greet(name) {
-    if (name) {
-        console.log(name.toUpperCase());
-    }
-    else {
-        console.log('hola');
-    }
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
 }
-greet(undefined);
+let customer = getCustomer(0);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
-function greet(name: string|null|undefined) {
-  if (name) {
-    console.log(name.toUpperCase());
-  } else {
-    console.log('hola')
-  }
+type Customer = {
+  birthday?: Date;
+};
+
+function getCustomer(id: number): Customer | null  |undefined {
+  return id === 0 ? null : { birthday: new Date() };
 }
 
-greet(undefined);
+let customer = getCustomer(0);
+//optional porperty access operator
+console.log(customer?.birthday?.getFullYear());
+
+//optional element access operator
+// if(customers!==null&&customers!==undifine)
+// customers[0]
+
+// customers?[0]
+
+//optional call operator
+let log: any = null;
+log?.('a');
