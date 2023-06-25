cb45d9ecfd6a7c49f51441e208a393347ab50877
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-25 13:09:03 +0530

    nullish operator

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 "use strict";
-function getCustomer(id) {
-    return id === 0 ? null : { birthday: new Date() };
-}
-let customer = getCustomer(0);
-console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
+let speed = null;
+let ride = {
+    speed: speed !== null && speed !== void 0 ? speed : 30
+};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,6 @@
-type Customer = {
-  birthday?: Date;
-};
-
-function getCustomer(id: number): Customer | null  |undefined {
-  return id === 0 ? null : { birthday: new Date() };
+let speed: number | null = null;
+let ride = {
+  // Falsy (undefined,null,'',false,0)
+  // Nullish coalescing operator
+  speed: speed ?? 30
 }
-
-let customer = getCustomer(0);
-//optional porperty access operator
-console.log(customer?.birthday?.getFullYear());
-
-//optional element access operator
-// if(customers!==null&&customers!==undifine)
-// customers[0]
-
-// customers?[0]
-
-//optional call operator
-let log: any = null;
-log?.('a');
