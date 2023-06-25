d37d011bdd5f4f051423f42b9a18c62d884bfebb
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-25 20:48:47 +0530

    parameter properties

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 class Account {
-  readonly id: number;
-  owner: string;
-  private _balance: number;
   nickName?: string;
-  constructor(id:number,owner:string,balance:number) {
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
-  }
-  deposit(amount: number): void{
+  constructor(
+    public readonly id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
+  deposit(amount: number): void {
     if (amount <= 0) {
-      throw new Error('invalid amount');
+      throw new Error("invalid amount");
     }
     // Record a transaction
-    this._balance += amount; 
+    this._balance += amount;
   }
-  getBalance(): number{
+  getBalance(): number {
     return this._balance;
   }
 }
 
-let account = new Account(1, 'Hanish', 10);
-console.log(account.getBalance())
+let account = new Account(1, "Hanish", 10);
+console.log(account.getBalance());
