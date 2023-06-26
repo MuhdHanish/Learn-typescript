ee5af57bfb4f9d2bca8f3d6c6e3622c050aba8a4
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-26 19:39:11 +0530

    exported js into ts and converted to js

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,6 @@
 "use strict";
-var _a, _b;
-function fetch(url) {
-    console.log(url);
-    return {
-        data: null,
-        error: null
-    };
-}
-let username = fetch('url');
-(_a = username.data) === null || _a === void 0 ? void 0 : _a.username;
-let product = fetch('url');
-(_b = product.data) === null || _b === void 0 ? void 0 : _b.title;
+Object.defineProperty(exports, "__esModule", { value: true });
+const tax_js_1 = require("./tax.js");
+let tax = (0, tax_js_1.calculateTax)(1000);
+console.log(tax);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,3 @@
-interface Product {
-  name: string;
-  price: number;
-}
-
-class Store<T> {
-  protected _objects: T[] = [];
-  add(obj: T): void {
-    this._objects.push(obj);
-  }
-}
-
-// pass on the generic type parameter
-class CompressibleStore<T> extends Store<T> {
-  compress() {}
-}
-
-// let store = new CompressibleStore<Product>();
-// store.compress();
-
-// Restrict the generic type parameter
-class SearchableStore<T extends { name: string }> extends Store<T> {
-  find(name: string): T | undefined {
-    return this._objects.find((obj) => obj.name === name);
-  }
-}
-
-// Fix the generic type parameter
-class ProductStore extends Store<Product> {
-  filterByCategory(category: string): Product[]{
-    return [];
-  }
-}
-
+import { calculateTax } from "./tax.js"
+let tax = calculateTax(1000);
+console.log(tax)
