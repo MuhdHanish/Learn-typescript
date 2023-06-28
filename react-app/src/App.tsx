287f5c0b7538d9a2890a0dec75c556ee3c92c121
MuhdHanish <muhammedhanish11@gmail.com>
2023-06-28 11:15:30 +0530

    delete function passed through the props'

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReminderList from './components/ReminderList';
@@ -6,15 +6,20 @@ import Reminder from './models/Reminder';
 import reminderService from './services/reminder';
 
 function App() {
-  const [reminders, setReminders] = useState<Reminder[]>([
-    {
-      id: 1,
-      title: "reminder1",
-    },
-  ]);
+  const [reminders, setReminders] = useState<Reminder[]>([]);
+  const loadReminders = async() => {
+    const reminders = await reminderService.getReminder();
+    setReminders(reminders);
+  }
+  useEffect(() => {
+    loadReminders();
+  }, []);
+  const removeReminder = (id: number) => {
+    console.log(id);
+  }
   return (
     <div className="App">
-      <ReminderList items={reminders} />
+      <ReminderList items={reminders} onRemoveReminder={removeReminder} />
     </div>
   );
 }
--- react-app/src/components/ReminderList.tsx
+++ react-app/src/components/ReminderList.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import Reminder  from '../models/Reminder';
+import Reminder from '../models/Reminder';
 
-
-interface ReminderListProps{
- items: Reminder[]
+interface ReminderListProps {
+  items: Reminder[];
+  onRemoveReminder: (id: number) => void;
 }
 
-function ReminderList(props:ReminderListProps) {
- return (
-  <ul>
-   {
-    props.items.map(item => <li key={item.id}>{ item.title }</li>)
-   }
-  </ul>
- );
+function ReminderList(props: ReminderListProps) {
+  return (
+    <ul className='list-group'>
+      {props.items.map(item => (
+        <li key={item.id} className='list-group-item'>
+          {item.title}
+          <button className="btn btn-outline-danger mx-2 rounded-pill"
+            onClick={() => props.onRemoveReminder(item.id)}
+          >
+            delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default ReminderList;
